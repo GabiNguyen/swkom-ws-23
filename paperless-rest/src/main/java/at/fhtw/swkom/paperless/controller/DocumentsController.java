eba3fe905246e2ef0de8515fdd998daf194fc473
stefi <if21b052@technikum-wien.at>
2023-12-02 17:10:37 +0100

    upload document

--- paperless-rest/src/main/java/at/fhtw/swkom/paperless/controller/DocumentsController.java
+++ paperless-rest/src/main/java/at/fhtw/swkom/paperless/controller/DocumentsController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -42,9 +43,18 @@ public class DocumentsController {
         return documentService.getAllDocuments();
     }
 
-    @PostMapping("/post_document")
-    public Document postDocuments(@RequestBody Document document) {
-        return documentService.postDocument(document);
+    @RequestMapping(
+        method = RequestMethod.POST,
+        value = "/post_document/",
+        consumes = { "multipart/form-data" }
+    )
+    public List<Document> postDocuments(@Parameter(name = "document", description = "") @RequestPart(value = "document", required = false) List<MultipartFile> document) {
+        List<Document> documents = new ArrayList<>();
+        for(int i=0; i<document.size(); i++){
+            Document doc = new Document(document.get(0).getOriginalFilename(), document.get(0).getContentType());
+            documents.add(documentService.postDocument(doc));
+        }
+        return documents;
     }
 
 }
