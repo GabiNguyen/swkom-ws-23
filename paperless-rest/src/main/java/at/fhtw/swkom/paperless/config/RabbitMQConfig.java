a39b956a37eb2e375b5cf4bcaaeb72fdefcb6c72
Gabi Nguyen <100677997+GabiNguyen@users.noreply.github.com>
2023-12-10 21:17:54 +0100

    tried implementing OCR worker

--- paperless-rest/src/main/java/at/fhtw/swkom/paperless/config/RabbitMQConfig.java
+++ paperless-rest/src/main/java/at/fhtw/swkom/paperless/config/RabbitMQConfig.java
@@ -11,6 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
+    public static final String EXCHANGE = "";
+
+    public static final String OCR_IN_QUEUE_NAME = "OCR_In";
+    public static final String OCR_OUT_QUEUE_NAME = "OCR_Out";
+    public static final String DOCUMENT_STORAGE_PATH_PROPERTY_NAME = "FileStoragePath";
+
+
     @Value("${rabbitmq.queue.name}")
     private String queue;
 
@@ -34,4 +41,45 @@ public class RabbitMQConfig {
     public Binding binding(){
         return BindingBuilder.bind(queue()).to(exchange()).with(routing_key);
     }
+
+    @Bean
+    public Queue ocrInQueue() {
+        return new Queue(OCR_IN_QUEUE_NAME, false);
+    }
+
+    @Bean
+    public Queue ocrOutQueue() { return new Queue(OCR_OUT_QUEUE_NAME, false); }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setDefaultReceiveQueue(OCR_IN_QUEUE_NAME);
+        return rabbitTemplate;
+    }
+
+    // Custom error handling with Spring AMQP
+    // see <a href="https://www.baeldung.com/spring-amqp-error-handling">Error Handling with Spring AMQP</a>
+    // Necessary to avoid requeuing of messages with JSON-parse exceptions
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setErrorHandler(errorHandler());
+        return factory;
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ServicesErrorHandler();
 }
---
+++ paperless-rest/src/test/java/at/fhtw/swkom/paperless/services/OCRServiceTest.java
@@ -0,0 +1,12 @@
+//package at.fhtw.swkom.paperless.services;
+//
+//import net.sourceforge.tess4j.Tesseract;
+//
+//@SpringBootTest
+//class OCRServiceTest {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
